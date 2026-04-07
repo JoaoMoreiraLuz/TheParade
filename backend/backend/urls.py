2edfa24e22ffd1abeefdd17c6c3e8ae021f8cd72
JoaoMoreiraLuz <joaogustavo9992@outlook.com>
2026-04-06 21:59:35 -0300

    quick fix

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
 
-from backend.base import views
+from ..base import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
